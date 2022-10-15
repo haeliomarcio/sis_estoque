5e4ff257bd84cd43a18dabf3c8f134fa17b8a6e9
Márcio Ferreira <haelioferreira@yahoo.com.br>
2022-10-15 10:02:29 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from './layouts/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Usuarios from './pages/Usuarios';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Usuarios />} />
+          <Route path="/" element={<Login />} />
           <Route path="/usuarios" element={<Usuarios />} />
         </Routes>
       </BrowserRouter>
